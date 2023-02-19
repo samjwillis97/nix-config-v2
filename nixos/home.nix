c2444b38bb4a306910c85de1b5289987d1f5a1dc
Sam Willis <sam@williscloud.org>
2023-02-19 01:49:50 -0500

    refactored looking at thiagokokada config

--- nixos/home.nix
+++ nixos/home.nix
@@ -6,19 +6,10 @@
     ../modules/meta.nix
   ];
 
-  options.nixos.home = {
-    enable = pkgs.lib.mkDefaultOption "home config";
-    username = lib.mkOption {
-      description = "Main username";
-      type = lib.types.str;
-      default = config.meta.username;
-    };
-  };
-
-  config = lib.mkIf config.nixos.home.enable {
+  config = {
     home-manager = {
       useUserPackages = true;
-      users.${config.nixos.home.username} = ../home-manager/nixos.nix;
+      users.${config.nixos.home.username} = ../home-manager/default;
       extraSpecialArgs = {
         inherit flake system;
         super = config;
