3fe075c36ab75b4b4b135c97ad5966fd7b31ab62
samjwillis97 <sam@williscloud.org>
2023-02-26 19:23:30 +1100

    Test out teams, removed for now.

--- home-manager/work/default.nix
+++ home-manager/work/default.nix
@@ -3,7 +3,11 @@
 { pkgs, ... }:
 {
     home.packages = with pkgs; [
-        # Clashing or something?
+        # FIXME:
+        # Commenting out for a bit.. MS killed the download URL and haven't done anything about it :) 
+        # See: https://github.com/NixOS/nixpkgs/issues/217473
+        # FIX: 
+        #  nix-prefetch-url 'https://web.archive.org/web/20221130115842if_/https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.5.00.23861_amd64.deb' 'sha256-h0YnCeJX//l4TegJVZtavV3HrxjYUF2Fa5KmaYmZW8E='
         /* teams */
     ];
 
