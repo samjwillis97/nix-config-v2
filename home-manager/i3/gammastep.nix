07399af52a7c119387813e797f17f1270b5d35d5
samjwillis97 <sam@williscloud.org>
2023-02-26 12:03:53 +1100

    Gammastep better during the day now.

--- home-manager/i3/gammastep.nix
+++ home-manager/i3/gammastep.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, ... }:
 {
+    # NEED TO PLAY AROUND WITH THE COLORS FIRST 
     services.gammastep = {
         enable = true;
         tray = true;
@@ -7,12 +8,12 @@
         duskTime = "19:30-20:30";
         package = pkgs.gammastep;
         temperature = {
-            day = 5700;
+            day = 6500;
             night = 3700;
         };
         settings = {
             general = {
-                gamma = 0.8;
+                gamma = 1;
                 fade = 1;
             };
         };
