1e41813fd5501c4a39d4b894778e9f4074db9d79
samjwillis97 <sam@williscloud.org>
2023-05-03 08:47:11 +1000

    added support for wifi to desktop

--- hosts/personal-desktop/hardware-configuration.nix
+++ hosts/personal-desktop/hardware-configuration.nix
@@ -4,41 +4,39 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/f407f015-f752-41df-8ace-b5c1c5aa1074";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/f407f015-f752-41df-8ace-b5c1c5aa1074";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4E31-0D82";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/4E31-0D82";
+    fsType = "vfat";
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/a83c32c8-5ac8-425b-be31-de71de85d6e9"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/a83c32c8-5ac8-425b-be31-de71de85d6e9"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  networking.wireless.enable = false;
+  networking.networkmanager.enable = true;
   networking.interfaces.enp9s0.useDHCP = lib.mkDefault true;
-  networking.nameservers = [
-    "1.1.1.1"
-    "8.8.8.8"
-  ];
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
   # networking.interfaces.wlp7s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
