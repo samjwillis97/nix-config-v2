5df0ae4d7a68f8aaffbabc60f17a256ddcd922b5
Sam Willis <sam@williscloud.org>
2023-05-17 21:59:45 +1000

    Added ncdu to linux os'

--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -1,26 +1,29 @@
-{ config, lib, pkgs, ... }: {
+{ super, config, lib, pkgs, ... }:
+let
+  osSpecificPackages = if super.meta.isDarwin then [ ] else with pkgs; [ ncdu ];
+in {
   imports = [ ./zsh.nix ./tmux.nix ./git.nix ./ssh.nix ../scripts ];
 
-  home.packages = with pkgs; [
-    bat
-    curl
-    jq
-    p7zip
-    ripgrep
-    wget
-    zip
-    unzip
-    difftastic
-    htop
-    fzf
-    direnv
-    neofetch
-    _1password
-    my-neovim
-    ncdu
+  home.packages = with pkgs;
+    [
+      bat
+      curl
+      jq
+      p7zip
+      ripgrep
+      wget
+      zip
+      unzip
+      difftastic
+      htop
+      fzf
+      direnv
+      neofetch
+      _1password
+      my-neovim
 
-    nodePackages.wrangler
-  ];
+      nodePackages.wrangler
+    ] ++ osSpecificPackages;
 
   programs.bat = {
     enable = true;
