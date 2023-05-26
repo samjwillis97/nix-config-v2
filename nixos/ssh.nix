8ca03ed4673962a46e13458f0afcb8f6d77e859d
samjwillis97 <sam@williscloud.org>
2023-05-26 21:16:57 +1000

    Updated SSH to use authorizedKeys :)

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -1,13 +1,13 @@
 { config, lib, pkgs, ... }:
 let inherit (config.meta) username;
-in
-{
-    services.openssh = {
-        enable = true;
-        settings.passwordAuthentication = true;
-    };
+in {
+  services.openssh = {
+    enable = true;
+    settings.passwordAuthentication = false;
+    # TODO: Look at ghuntley configuration - looks like it only allows tailscale which is ideal
+  };
 
-    # TODO: SSH Keys
+  # TODO: SSH Keys
 
-    programs.mosh.enable = true;
+  programs.mosh.enable = true;
 }
--- nixos/user.nix
+++ nixos/user.nix
@@ -1,8 +1,6 @@
 { config, lib, pkgs, ... }:
-let
-  inherit (config.meta) username;
-in
-{
+let inherit (config.meta) username;
+in {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${username} = {
     isNormalUser = true;
@@ -10,5 +8,9 @@ in
     extraGroups = [ "wheel" "networkmanager" "video" "docker" ];
     shell = pkgs.zsh;
     password = "nixos";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2FeFN6YQEUr22lJCeuQHcDawLuAPnoizlZLJOwhch4 sam@williscloud.org"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYyMM/qTTLsXdPvvfkhdufg9gLYOI2y8d1oDpAgI0ft samjwillis97@gmail.com"
+    ];
   };
 }
