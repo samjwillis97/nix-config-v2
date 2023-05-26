94d7993623053353e2755524b3489760cb92e3f9
samjwillis97 <sam@williscloud.org>
2023-05-26 19:43:57 +1000

    Adding secret to test on macos.

---
+++ secrets/secrets.nix
@@ -0,0 +1,8 @@
+let
+  personal-system =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2FeFN6YQEUr22lJCeuQHcDawLuAPnoizlZLJOwhch4 sam@williscloud.org";
+  personal-laptop =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYyMM/qTTLsXdPvvfkhdufg9gLYOI2y8d1oDpAgI0ft samjwillis97@gmail.com";
+
+  default.publicKeys = [ personal-system personal-laptop ];
+in { "tailscale_pre-auth.age" = default; }
