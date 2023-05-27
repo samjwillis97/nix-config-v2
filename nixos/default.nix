02f1f1778a414336e2e1fee838b51b29ad4ee096
Sam Willis <sam@williscloud.org>
2023-05-27 17:15:23 +1000

    Added some system packages for my barebones systems.

--- nixos/default.nix
+++ nixos/default.nix
@@ -3,4 +3,6 @@ let homeManager = if super.meta.useHomeManager then [ ./home.nix ] else [ ];
 in {
   imports = [ ./meta.nix ./user.nix ./ssh.nix ./locale.nix ./tailscale.nix ]
     ++ homeManager;
+
+  environment.systemPackages = with pkgs; [ vim git wget ];
 }
