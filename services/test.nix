d3c4f2ab2a5b3af7ecc1f8ef4f83038d7b06c51c
Sam Willis <sam@williscloud.org>
2023-05-28 12:13:06 +1000

    Another test 😠

--- services/test.nix
+++ services/test.nix
@@ -15,15 +15,6 @@
     locations."/" = {
       extraConfig = ''
         proxy_pass http://localhost:8080;
-        proxy_pass_header Authorization;
-        proxy_http_version 1.1;
-        proxy_ssl_server_name on;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header Host $host;
       '';
     };
   };
