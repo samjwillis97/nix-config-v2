c61898ff33327a408302b4102bd727602bfa88f1
Sam Willis <sam@williscloud.org>
2023-05-28 12:47:56 +1000

    Really shitty..

--- services/test.nix
+++ services/test.nix
@@ -1,38 +1,28 @@
 { super, ... }: {
-  services.caddy = {
+  services.nginx = {
     enable = true;
-    virtualHosts."${super.meta.hostname}".extraConfig = ''
-      reverse_proxy http://localhost:8080
-    '';
+
+    # Use recommended settings
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
   };
-  # services.nginx = {
-  #   enable = true;
 
-  #   # Use recommended settings
-  #   recommendedGzipSettings = true;
-  #   recommendedOptimisation = true;
-  #   recommendedProxySettings = true;
-  # };
+  services.nginx.virtualHosts."nginx.${super.meta.hostname}" = {
+    forceSSL = false;
+    enableACME = false;
 
-  # services.nginx.virtualHosts."nginx.${super.meta.hostname}" = {
-  #   forceSSL = false;
-  #   enableACME = false;
-
-  #   locations."/" = {
-  #     extraConfig = ''
-  #       proxy_pass http://localhost:8080;
-  #       proxy_pass_header Authorization;
-  #       proxy_http_version 1.1;
-  #       proxy_ssl_server_name on;
-  #       proxy_set_header Upgrade $http_upgrade;
-  #       proxy_set_header Connection "upgrade";
-  #       proxy_set_header X-Real-IP $remote_addr;
-  #       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-  #       proxy_set_header X-Forwarded-Proto $scheme;
-  #       proxy_set_header Host $host;
-  #     '';
-  #   };
-  # };
+    locations."/" = {
+      extraConfig = ''
+        proxy_pass http://localhost:8080;
+        proxy_http_version 1.1;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host $host;
+      '';
+    };
+  };
 
   virtualisation.oci-containers.containers = {
     hello-world = {
