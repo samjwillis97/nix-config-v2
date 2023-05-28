5faf750c7ef975703838d18f6c82332550edf690
Sam Willis <sam@williscloud.org>
2023-05-28 13:25:45 +1000

    trying again 🤡

--- services/test.nix
+++ services/test.nix
@@ -9,21 +9,16 @@
     recommendedProxySettings = true;
   };
 
-  services.nginx.virtualHosts."nginx.${super.meta.hostname}" = {
-    forceSSL = false;
-    enableACME = false;
+  services.nginx.virtualHosts."nginx.${super.meta.hostname}.tailfba7c.ts.net" =
+    {
+      forceSSL = false;
+      enableACME = false;
 
-    locations."/" = {
-      extraConfig = ''
-        proxy_pass http://localhost:8080;
-        proxy_http_version 1.1;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header Host $host;
-      '';
+      locations."/" = {
+        proxyPass = "http://localhost:8080";
+        recommendedProxySettings = true;
+      };
     };
-  };
 
   virtualisation.oci-containers.containers = {
     hello-world = {
