02b6d99e4355de8954b10383ac54f2713f401a26
Sam Willis <sam@williscloud.org>
2023-05-29 21:20:06 +1000

    Putting back host network

--- services/coder.nix
+++ services/coder.nix
@@ -43,6 +43,7 @@ in {
     coder = {
       image = "ghcr.io/coder/coder:latest";
       user = "root";
+      extraOptions = [ "--network=host" ];
       ports = [ "3000:3000" ];
       environment = {
         CODER_ACCESS_URL = "http://${super.meta.hostname}.tailfba7c.ts.net";
