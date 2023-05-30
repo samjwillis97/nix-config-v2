9f276b33141f57e5dd0f1eb2f4190194db0009d1
Sam Willis <sam@williscloud.org>
2023-05-30 20:39:04 +1000

    Fixed coder hostname..

--- services/coder/default.nix
+++ services/coder/default.nix
@@ -46,6 +46,7 @@ in {
     containers = {
       coder = {
         image = "ghcr.io/coder/coder:latest";
+        hostname = "coder";
         user = "root";
         extraOptions = [ "--network=host" ];
         ports = [ "3000:3000" ];
