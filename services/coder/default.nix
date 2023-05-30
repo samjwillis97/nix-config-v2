26cc743d82cedf3800b5d29be7dc10b042fbce2b
Sam Willis <sam@williscloud.org>
2023-05-30 19:43:01 +1000

    Lets fuck around and find out hey.

--- services/coder/default.nix
+++ services/coder/default.nix
@@ -39,9 +39,10 @@ in {
     };
   };
 
+  virtualisation.docker.enable = true;
   # See: https://github.com/ghuntley/ghuntley/blob/c234e2180693304bc6ea17c4862bbb2f807e8727/.github/workflows/services-dev-ghuntley-templates-push.yml#L12
   virtualisation.oci-containers = {
-    backend = "docker";
+    backend = "podman";
     containers = {
       coder = {
         image = "ghcr.io/coder/coder:latest";
