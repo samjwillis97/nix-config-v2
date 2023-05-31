f62906d10fb40a5ac5494f2001468df374d9caa3
Sam Willis <sam@williscloud.org>
2023-05-31 20:47:36 +1000

    Better cachix setups. I hope.

--- cachix.nix
+++ cachix.nix
@@ -9,6 +9,7 @@ let
     (lib.filterAttrs filterCaches (builtins.readDir folder));
 in {
   inherit imports;
-  nix.settings.substituters =
-    [ "https://cache.nixos.org/" "https://nix-community.cachix.org" ];
+  nix.settings.trusted-public-keys =
+    [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+  nix.settings.substituters = [ "https://cache.nixos.org/" ];
 }
