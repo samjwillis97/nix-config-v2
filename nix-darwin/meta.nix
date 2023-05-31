0eb6a741723359471b0722566108b3024e54fd51
Sam Willis <sam@williscloud.org>
2023-05-31 21:23:10 +1000

    Well these were missing still need to add to home-manager I would say..

--- nix-darwin/meta.nix
+++ nix-darwin/meta.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: {
+  imports = [ ../overlays ../cachix.nix ];
+  environment.systemPackages = with pkgs; [ cachix ];
   services.nix-daemon.enable = true;
   nixpkgs.config.allowUnfree = true;
 }
