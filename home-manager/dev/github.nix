04c1aff87369422186b22f0118a504cad7f617b8
samjwillis97 <sam@williscloud.org>
2023-06-02 12:00:28 +1000

    Added some stuffs

--- home-manager/dev/github.nix
+++ home-manager/dev/github.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ act ];
+  home.packages = with pkgs; [ act nix-build-uncached ];
 
   # TODO: Get mongodb-memory-server working by exporting the path
   # environment.variables.MONGOMS_SYSTEM_BINARY= "${pkgs.mongodb-5_0}/bin/mongod";
