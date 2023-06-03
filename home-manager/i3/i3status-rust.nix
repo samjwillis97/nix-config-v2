d3aec34f2150b0c1b88b62ab4ca7283c67f24f1c
samjwillis97 <sam@williscloud.org>
2023-06-03 14:42:42 +1000

    Fixed i3status-rust config.. just commented out offending lines :)

--- home-manager/i3/i3status-rust.nix
+++ home-manager/i3/i3status-rust.nix
@@ -1,115 +1,109 @@
-{ super, config, lib, pkgs, ... }:
-{
-    programs.i3status-rust = {
-        enable = true;
-        package = pkgs.i3status-rust;
-        bars =
-            let
-                settings = {
-                    theme = {
-                        name = "plain";
-						overrides = with config.theme.colors; {
-                            idle_bg = base00;
-                            idle_fg = base05;
-                            info_bg = base0D;
-                            info_fg = base00;
-                            good_bg = base00;
-                            good_fg = base05;
-                            warning_bg = base0A;
-                            warning_fg = base00;
-                            critical_bg = base08;
-                            critical_fg = base00;
-                            separator_bg = base00;
-                            separator = " ";
-                        };
-                    };
-                    icons = {
-                        name = "awesome5";
-                        overrides = {
-                            memory_swap = " ";
-                            disk_drive = " ";
-                            caffeine_on = "  ";
-                            caffeine_off = "  ";
-                            notification_on = "  ";
-                            notification_off = "  ";
-                        };
-                    };
-                };
+{ super, config, lib, pkgs, ... }: {
+  programs.i3status-rust = {
+    enable = true;
+    package = pkgs.i3status-rust;
+    bars = let
+      settings = {
+        theme = {
+          theme = "plain";
+          overrides = with config.theme.colors; {
+            idle_bg = base00;
+            idle_fg = base05;
+            info_bg = base0D;
+            info_fg = base00;
+            good_bg = base00;
+            good_fg = base05;
+            warning_bg = base0A;
+            warning_fg = base00;
+            critical_bg = base08;
+            critical_fg = base00;
+            separator_bg = base00;
+            separator = " ";
+          };
+        };
+        icons = {
+          icons = "awesome5";
+          overrides = {
+            memory_swap = " ";
+            disk_drive = " ";
+            caffeine_on = "  ";
+            caffeine_off = "  ";
+            notification_on = "  ";
+            notification_off = "  ";
+          };
+        };
+      };
 
-                netBlock = {
-                    block = "net";
-                    device = "${super.meta.networkAdapterName}";
-                    format = "{ip} {speed_down;K*b} {graph_down;K*b}";
-                    interval = 5;
-                };
+      netBlock = {
+        block = "net";
+        device = "${super.meta.networkAdapterName}";
+        # format = "{ip} {speed_down;K*b} {graph_down;K*b}";
+        # interval = 5;
+      };
 
-                diskBlock = {
-                    block = "disk_space";
-                    path = "/";
-                    alias = "/";
-                    info_type = "available";
-                    unit = "GB";
-                    interval = 20;
-                    warning = 20.0;
-                    alert = 10.0;
-                };
+      diskBlock = {
+        block = "disk_space";
+        path = "/";
+        # info_type = "available";
+        # unit = "GB";
+        # interval = 20;
+        # warning = 20.0;
+        # alert = 10.0;
+      };
 
-                memoryBlock = {
-                    block = "memory";
-                    display_type = "memory";
-                    format_mem = "{mem_used_percents}";
-                    format_swap = "{swap_used_percents}";
-                };
+      memoryBlock = {
+        block = "memory";
+        # display_type = "memory";
+        # format_mem = "{mem_used_percents}";
+        # format_swap = "{swap_used_percents}";
+      };
 
-                cpuBlock = {
-                    block = "cpu";
-                    interval = 1;
-                };
+      cpuBlock = {
+        block = "cpu";
+        interval = 1;
+      };
 
-                loadBlock = {
-                    block = "load";
-                    interval = 1;
-                    format = "{1m}";
-                };
+      loadBlock = {
+        block = "load";
+        interval = 1;
+        format = "{1m}";
+      };
 
-                soundBlock = {
-                    block = "sound";
-                };
+      soundBlock = { block = "sound"; };
 
-                timeBlock = {
-                    block = "time";
-                    interval = 5;
-                    format = "%a %d/%m %R";
-                };
-            in
-            {
-                i3 = {
-                    inherit settings;
+      timeBlock = {
+        block = "time";
+        # interval = 5;
+        # format = "%a %d/%m %R";
+      };
+    in {
+      i3 = {
+        inherit settings;
 
-                    blocks = lib.lists.flatten [
-                        netBlock
-                        diskBlock
-                        memoryBlock
-                        cpuBlock
-                        loadBlock
-                        soundBlock
-                        timeBlock
-                    ];
-                };
+        blocks = lib.lists.flatten [
+          netBlock
+          diskBlock
+          memoryBlock
+          cpuBlock
+          loadBlock
+          soundBlock
+          timeBlock
+        ];
+      };
 
-                sway = {
-                    inherit settings;
+      sway = {
+        inherit settings;
 
-                    blocks = lib.lists.flatten [
-                        netBlock
-                        diskBlock
-                        memoryBlock
-                        cpuBlock
-                        loadBlock
-                        soundBlock
-                        timeBlock
-                    ];
-                };
-            };
+        blocks = lib.lists.flatten [
+          netBlock
+          diskBlock
+          memoryBlock
+          cpuBlock
+          loadBlock
+          soundBlock
+          timeBlock
+        ];
+      };
     };
+  };
 }
