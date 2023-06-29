8b039eb0cd633e9eaad9b5c6ea7e50692c3af30f
Sam Willis <sam@williscloud.org>
2023-06-29 13:32:12 +1000

    Added microvm cachix.

---
+++ cachix/microvm.nix
@@ -0,0 +1,7 @@
+{
+  nix.settings = {
+    substituters = [ "https://microvm.cachix.org" ];
+    trusted-public-keys =
+      [ "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys=" ];
+  };
+}
