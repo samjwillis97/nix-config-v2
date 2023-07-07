924e60a2209e775deb5133385dcd9dc8d8d1de2d
samjwillis97 <sam@williscloud.org>
2023-07-07 16:52:58 +1000

    Added coder install to docker image.

--- services/coder/templates/base/main.tf
+++ services/coder/templates/base/main.tf
@@ -30,9 +30,6 @@ resource "coder_agent" "main" {
   startup_script_timeout = 180
   startup_script         = <<-EOT
     set -e
-
-    # install and start code-server
-    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
     /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
   EOT
 
