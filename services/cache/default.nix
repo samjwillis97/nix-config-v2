1372e70360479044776b056a206ae9f8dd7c4ef7
samjwillis97 <sam@williscloud.org>
2023-07-08 14:43:43 +1000

    Alright try 2

--- services/cache/default.nix
+++ services/cache/default.nix
@@ -1,7 +1,16 @@
-{ ... }: {
+{ super, ... }:
+{
   services.nix-serve = {
     enable = true;
     secretKeyFile = "/var/cache-priv-key.pem";
-    port = 80;
+    port = 5000;
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts.${super.meta.hostname} = {
+      locations."/".proxyPass = "http://localhost:5000";
+    };
   };
 }
