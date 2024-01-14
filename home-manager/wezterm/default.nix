e581744e5bd56ae9c1bef57ab045063e10da35d1
samjwillis97 <sam@williscloud.org>
2024-01-15 08:15:25 +1100

    Added keybinds to adjust wezterm opacity

--- home-manager/wezterm/default.nix
+++ home-manager/wezterm/default.nix
@@ -17,12 +17,38 @@
     enableZshIntegration = true;
     # `text_background_opacity` effects the background colors of neovim
     extraConfig = ''
+      wezterm.on('increase-opacity', function(window, pane)
+        local overrides = window:get_config_overrides() or {}
+        local config = window:effective_config() or {}
+        overrides.window_background_opacity = config.window_background_opacity + 0.025
+        window:set_config_overrides(overrides)
+      end)
+
+      wezterm.on('decrease-opacity', function(window, pane)
+        local overrides = window:get_config_overrides() or {}
+        local config = window:effective_config() or {}
+        overrides.window_background_opacity = config.window_background_opacity - 0.025
+        window:set_config_overrides(overrides)
+      end)
+
       return {
+        keys = {
+          {
+            key = ']',
+            mods = 'CTRL',
+            action = wezterm.action.EmitEvent 'increase-opacity',
+          },
+          {
+            key = '[',
+            mods = 'CTRL',
+            action = wezterm.action.EmitEvent 'decrease-opacity',
+          },
+        },
         font = wezterm.font("FiraCode Nerd Font Mono"),
         hide_tab_bar_if_only_one_tab = true,
         color_scheme = "base16",
         text_background_opacity = 1,
-        window_background_opacity = 0.8,
+        window_background_opacity = 0.85,
         macos_window_background_blur = 15,
         window_decorations = "RESIZE",
         harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
