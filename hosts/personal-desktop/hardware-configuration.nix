602108bd1fa1b640476c1fb42be6f7f838f4d15c
samjwillis97 <sam@williscloud.org>
2024-02-07 13:23:48 +1100

    - Modified SSH keys - Fixed up qemu finally was actually a bios issue

--- hosts/personal-desktop/hardware-configuration.nix
+++ hosts/personal-desktop/hardware-configuration.nix
@@ -13,10 +13,12 @@
     };
   };
 
+  virtualisation.libvirtd.enable = true;
+
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-intel" "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
--- nixos/user.nix
+++ nixos/user.nix
@@ -5,13 +5,13 @@ in {
   users.users.${username} = {
     isNormalUser = true;
     uid = 1000;
-    extraGroups = [ "wheel" "networkmanager" "video" "docker" ];
+    extraGroups = [ "wheel" "networkmanager" "video" "docker" "libvirtd" "qemu-libvirtd" ];
     shell = pkgs.zsh;
     password = "nixos";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2FeFN6YQEUr22lJCeuQHcDawLuAPnoizlZLJOwhch4 sam@williscloud.org"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYyMM/qTTLsXdPvvfkhdufg9gLYOI2y8d1oDpAgI0ft samjwillis97@gmail.com"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDe5uIFvexscAC/HCsBwctMuoABqQ6CN7Wq8dYXSJqjAz8pFoW/kvyl5WtRa372LokUYflsyXvhWQJJWyJVwsC4JvkN44QyWLLVBlp21GCqyvILyk960hGYgiyauf96sV7w5Aq+NRibsPjK6n2PLmxt9U4Dcfg4LgX6sXKc5+rljoToXjf1DKeFcjIfIwwJXWFt2njCromt8WYQQxg0w+YL6b5Hqi5LZcYjQu+AF0AMptoTyh9J1KQQg+XZjdbM8z+vf0VRFJlBrrQHyNxgqeWhMwxipTVq1yr+henaba8Tag/iacP070RDX2qTvUqmtS0Cftt7FaW6zVK+27FHigKJPNjz/ExiWnmGCNA+t5MBz5EGfHdZRf0Vv4x82+eHkhGnigs0rBFcHDqGVSmHKld4WKzyeIyTecmXr9XcODoxR+xEhMqzw8fy3D9ZaZTalzDPDRlzONpD4J0oI8ly4N0nUn+gJzM02i6RsbrzM90CsVdCpFR2RsUZTIjc4ATKF+c= sam@AMP-8060"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENzw8pIt2UVGWcXUx4E4AxxWj8zA+DLZSp0y7RGK5VW samuel.willis@nib.com.au"
     ];
   };
 
