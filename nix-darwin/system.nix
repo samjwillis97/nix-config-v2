cc77211f827493121c17fc34482931fd0480d344
Sam Willis <sam@williscloud.org>
2024-02-19 08:36:04 +1100

    Fixed my shitty merging practices :)

--- nix-darwin/system.nix
+++ nix-darwin/system.nix
@@ -1,4 +1,3 @@
-{ config, lib, pkgs, ... }:
 {
     system = {
         defaults.CustomSystemPreferences = {
@@ -10,15 +9,5 @@
           # Disable mouse acceleration
           # "com.apple.mouse.scaling" = -1;
         };
-    };
-    defaults.CustomSystemPreferences = {
-      # Do not write .DS_Store files outside macOS
-      "com.apple.desktopservices" = {
-        DSDontWriteNetworkStores = true;
-        DSDontWriteUSBStores = true;
-      };
-      # Disable mouse acceleration
-      # "com.apple.mouse.scaling" = -1;
-    };
   };
 }
