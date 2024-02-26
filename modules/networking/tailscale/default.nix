554348f429f21e1f4345432f2abf521d01020ee1
samjwillis97 <sam@williscloud.org>
2024-02-26 23:12:22 +1100

    I've tried the tailscale refactor now to put it to the test

--- modules/networking/tailscale/default.nix
+++ modules/networking/tailscale/default.nix
@@ -1,3 +1,5 @@
+# thank you: https://tailscale.com/blog/nixos-minecraft/
+# and: https://github.com/ghuntley/ghuntley/blob/cb78de98fbaf1ea97d5c8465e155516f3e72132d/ops/nixos-modules/tailscale.nix
 { config, lib, ... }:
 with lib;
 let cfg = config.modules.networking.tailscale;
@@ -13,10 +15,65 @@ in {
       example = ''["10.0.0.0/24"]'';
       description = "List of routes to advertise";
     };
+
+    authKeyFile = mkOption {
+      type = with types; oneOf [ path str ];
+      description = "File containing authKey";
+      example = "/var/lib/myKey";
+    };
   };
 
   config = mkIf cfg.enable {
-    warnings  =
-      [ ''Module "services.tailscale" is still under construction'' ];
+    services.tailscale.enable = true;
+
+    networking.firewall = {
+      # enable the firewall
+      enable = true;
+
+      # always allow traffic from your Tailscale network
+      trustedInterfaces = [ "tailscale0" ];
+
+      # allow the Tailscale UDP port through the firewall
+      allowedUDPPorts = [ config.services.tailscale.port ];
+
+      checkReversePath = "loose";
+    };
+
+    boot.kernel.sysctl = mkIf cfg.allowExitNode {
+      "net.ipv6.conf.all.forwarding" = "1"; # for tailscale exit node
+    };
+
+    systemd.services.tailscale-autoconnect = {
+      description = "Automatic connection to Tailscale";
+
+      # make sure tailscale is running before trying to connect to tailscale
+      after = [ "network-pre.target" "tailscale.service" ];
+      wants = [ "network-pre.target" "tailscale.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      # set this service as a oneshot job
+      serviceConfig.Type = "oneshot";
+
+      # have the job run this shell script
+      script = with pkgs; ''
+        # wait for tailscaled to settle
+        sleep 2
+
+        if [ ! -f ${cfg.authKeyFile} ]; then
+            exit 0
+        fi
+
+        # check if we are already authenticated to tailscale
+        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+        if [ $status = "Running" ]; then # if so, then do nothing
+          exit 0
+        fi
+
+        key=`cat ${cfg.authKeyFile}`
+
+        # otherwise authenticate with tailscale
+        ${tailscale}/bin/tailscale up -authkey $key
+      '';
+    };
   };
 }
