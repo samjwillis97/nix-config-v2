178d1d2d0b35b6029411cd6cd577bf8105f8b816
samjwillis97 <sam@williscloud.org>
2024-07-30 14:50:07 +1000

    Backup working, and automatic restore if no db exists on host

--- hosts/microvms/paperless/default.nix
+++ hosts/microvms/paperless/default.nix
@@ -2,6 +2,10 @@
 let 
   paperlessDataDir = "/data";
   backupDirectory = "/backups";
+
+  dbFile = "${config.services.paperless.dataDir}/db.sqlite3";
+  dbBackupDirectory = "${backupDirectory}/${config.networking.hostName}";
+  dbBackupFile = "${dbBackupDirectory}/db.sqlite3";
 in
 {
   imports = [
@@ -58,16 +62,51 @@ in
   };
 
   # Make sure rclone mount is up first
-  systemd.services.paperless-scheduler.after = ["data.mount"];
-  systemd.services.paperless-consumer.after = ["data.mount"];
-  systemd.services.paperless-web.after = ["data.mount"];
+  systemd.services.paperless-scheduler.after = [ "data.mount" "backup.mount" "paperless-db-backup-check.service" ];
+  systemd.services.paperless-consumer.after = [ "data.mount" "backup.mount" "paperless-db-backup-check.service" ];
+  systemd.services.paperless-web.after = [ "data.mount" "backup.mount" "paperless-db-backup-check.service" ];
 
   # Backs up the SQLite database
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "0 * * * *    root    ${pkgs.rclone}/bin/rclone copy ${config.services.paperless.dataDir}/db.sqlite3 ${backupDirectory}/db"
+      "0 * * * *    root    ${pkgs.rclone}/bin/rclone copy ${dbFile} ${dbBackupDirectory}"
     ];
+  };
+
+  systemd.services.paperless-db-backup-check = {
+    description = "Copies db file from backup if one doesn't exist";
+
+    after = [ "backup.mount" ];
+    wants = [ "backup.mount" ];
+    wantedBy = [
+      "paperless-scheduler.service"
+      "paperless-consumer.service"
+      "paperless-web.service"
+    ];
+
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      dbFile=${dbFile}
+      dbBackupFile=${dbBackupFile}
+      if  [ ! -e "$dbFile" ]; then
+        echo "DB file does not exists"
+
+        if  [ ! -e "$dbBackupFile" ]; then
+          echo "DB Backup does not exist"
+        else
+          echo "DB Backup exists"
+          echo "Going to copy backup file"
+          ${pkgs.coreutils}/bin/cp "$dbBackupFile" "$dbFile"
+          ${pkgs.coreutils}/bin/chown ${config.services.paperless.user}:${config.services.paperless.user} "$dbFile"
+          ${pkgs.coreutils}/bin/chmod 600 "$dbFile"
+          echo "Backup copied"
+        fi
+      else
+        echo "DB file already exists, skipping backup copy"
+      fi
+    '';
   };
 
   services.nginx = {
