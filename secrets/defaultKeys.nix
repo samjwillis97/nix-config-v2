a8c56c392218132e6d42e69980f5bbea0b46aed4
Sam Willis <sam@williscloud.org>
2024-08-06 23:23:13 +1000

    Fixed up agenix attempting to decrypt un-needed serets

---
+++ secrets/defaultKeys.nix
@@ -0,0 +1,13 @@
+
+{
+  age = {
+    secrets = {
+      "tailscale_pre-auth" = {
+        file = ./tailscale_pre-auth.age;
+      };
+    };
+    identityPaths = [
+      "/var/agenix/id-ed25519-agenix-primary"
+    ];
+  };
+}
