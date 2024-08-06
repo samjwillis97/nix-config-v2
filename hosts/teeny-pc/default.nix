a816fd5fbe983c0456ff99596cefc998f0d3b128
Sam Willis <sam@williscloud.org>
2024-08-06 23:39:37 +1000

    Better user handling with a refactor, also removing un-needed stuff from teeny

--- hosts/teeny-pc/default.nix
+++ hosts/teeny-pc/default.nix
@@ -7,16 +7,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.networkmanager.enable = true;
-  networking.firewall.enable = false;
-
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "yes";
-    };
-  };
-
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "btrfs";
--- nixos/user.nix
+++ nixos/user.nix
@@ -1,37 +1,18 @@
 {
   super,
-  lib,
-  pkgs,
   ...
 }:
 let
-  inherit (super.meta) username;
+  inherit (super.meta) username useHomeManager;
 in
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${username} = {
-    isNormalUser = true;
-    uid = 1000;
-    shell = pkgs.zsh;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "video"
-      "docker"
-      "libvirtd"
-      "qemu-libvirtd"
-    ];
-    password = "nixos";
-    openssh = {
-      authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2FeFN6YQEUr22lJCeuQHcDawLuAPnoizlZLJOwhch4 sam@williscloud.org"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYyMM/qTTLsXdPvvfkhdufg9gLYOI2y8d1oDpAgI0ft samjwillis97@gmail.com"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENzw8pIt2UVGWcXUx4E4AxxWj8zA+DLZSp0y7RGK5VW samuel.willis@nib.com.au"
-      ];
-    };
-  };
+  imports = [ ../modules/system/users ];
 
-  users.groups.deployer = {};
+  modules.system.users.standardUser = {
+    enable = true;
+    username = username;
+    useHomeManager = useHomeManager;
+  };
 
   users.users.deployer = {
     isNormalUser = true;
