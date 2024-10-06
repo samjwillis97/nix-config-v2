07a8bc6eee4ec42d1ebae626cb2e0bd50f796e62
samjwillis97 <sam@williscloud.org>
2024-10-07 08:55:52 +1100

    Removing max file upload size limit

--- hosts/microvms/cache/default.nix
+++ hosts/microvms/cache/default.nix
@@ -96,6 +96,7 @@ in
       locations."/" = {
         proxyPass = "http://127.0.0.1:${toString port}";
         extraConfig = ''
+          client_max_body_size 0
           proxy_set_header    Upgrade     $http_upgrade;
           proxy_set_header    Connection  "upgrade";
         '';
