0e473926efa71ae983db85d7601da2444b4b31d9
samjwillis97 <sam@williscloud.org>
2024-10-08 08:54:41 +1100

    Okay fixing nix conf

--- hosts/github-runner/default.nix
+++ hosts/github-runner/default.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ flake, pkgs, ... }:
 {
   services.tailscale.enable = true;
 
   environment.systemPackages = with pkgs; [ tailscale ];
 
   services.nix-daemon.enable = true;
+
+  nix = import ../../shared/nix.nix { inherit pkgs flake; };
 
   system.stateVersion = 5;
 }
