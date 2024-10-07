0e2f767524f736dab072e1d518c2a669d52bbd79
samjwillis97 <sam@williscloud.org>
2024-10-07 13:24:59 +1100

    Updating wallpaper URL

--- packages/wallpapers/default.nix
+++ packages/wallpapers/default.nix
@@ -11,7 +11,7 @@ in
   nixos-catppuccin-magenta-blue = mkWallpaper {
     name = "nixos-catppuccin-magenta-blue";
     ext = "png";
-    url = "https://raw.githubusercontent.com/catppuccin/wallpapers/main/os/nix-magenta-blue-1920x1080.png";
+    url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/os/nix-magenta-blue-1920x1080.png";
     sha256 = "sha256-CsBF3h4p0EEawF9aNDzm9DN+YoxyEnicc9n0oC8FCfs=";
   };
 }
