7b95a6696d9e309530e10eedbfc0e8d4cff73629
samjwillis97 <sam@williscloud.org>
2025-05-28 11:33:55 +1000

    Updating firefox tings

--- home-manager/firefox/default.nix
+++ home-manager/firefox/default.nix
@@ -42,16 +42,11 @@ in
           {
             name = "Google Search";
             keyword = "g";
-            url = "https://www.google.com/search?q=%s";
-          }
-          {
-            name = "Google Search AU";
-            keyword = "ga";
             url = "https://www.google.com.au/search?q=%s";
           }
           {
             name = "Github Code Search";
-            keyword = "cs";
+            keyword = "ghcs";
             url = "https://cs.github.com/?scopeName=All+repos&scope=&q=%s";
           }
           {
@@ -69,7 +64,26 @@ in
             keyword = "osrs";
             url = "https://oldschool.runescape.wiki/?search=%s&title=Special%3ASearch&fulltext=Search";
           }
-          # TODO: Jira/Confluence/Bitbucket, HN, OZb, Twitter, Github, AWARE
+          {
+            name = "nib Jira Search";
+            keyword = "j";
+            url = "https://nibgroup.atlassian.net/issues/?jql=text~%22%s%22%20or%20description%20~%20%22%s%22%20or%20summary%20~%20%22%s%22";
+          }
+          {
+            name = "nib Confluence Search";
+            keyword = "c";
+            url = "https://nibgroup.atlassian.net/wiki/search/?text=%s";
+          }
+          {
+            name = "nib Github Search";
+            keyword = "ngh";
+            url = "https://github.com/nib-group?q=%s&type=&language=";
+          }
+          {
+            name = "nib Buildkite Search";
+            keyword = "bk";
+            url = "https://buildkite.com/nib-health-funds-ltd?filter=%s";
+          }
         ];
       };
       settings = {
