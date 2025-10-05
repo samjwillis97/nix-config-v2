a3738db8c28e2222268a7e24c1f8f352b70140eb
samjwillis97 <sam@williscloud.org>
2025-10-05 22:28:55 +1100

    Testing some nix tooling

--- home-manager/dev/ops.nix
+++ home-manager/dev/ops.nix
@@ -1,4 +1,12 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ deploy-rs ];
+  home.packages = with pkgs; [ 
+    deploy-rs
+
+    # Nix tools under testing
+    nox
+    nix-output-monitor
+    nvd
+    nh
+  ];
 }
