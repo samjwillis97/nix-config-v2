31cfdecd73e98ed6041c720377a89e594bc60972
samjwillis97 <sam@williscloud.org>
2025-10-06 20:38:25 +1100

    Changing directories

--- hosts/mini-media-server/default.nix
+++ hosts/mini-media-server/default.nix
@@ -35,6 +35,7 @@
       sonarr = {
         enable = true;
         openFirewall = true;
+        libraryDirectory = "/shows";
         database.postgres.enable = true;
         downloaders = {
           decypharr = true;
@@ -47,6 +48,7 @@
       radarr = {
         enable = true;
         openFirewall = true;
+        libraryDirectory = "/movies";
         database.postgres.enable = true;
         downloaders = {
           decypharr = true;
