56293a96d04859f9eeca4b63355950601319370a
samjwillis97 <sam@williscloud.org>
2026-05-16 10:00:18 +1000

    Trying to fix font sizes

--- nixos/theming.nix
+++ nixos/theming.nix
@@ -46,6 +46,13 @@
         package = pkgs.noto-fonts-color-emoji;
         name = "Noto Color Emoji";
       };
+
+      sizes = {
+        desktop = 10;
+        terminal = 12;
+        applications = 10;
+        popups = 10;
+      };
     };
 
     opacity = {
